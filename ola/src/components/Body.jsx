ac43062896ad3b6a979c1a471d9f91799219d260
Carati32 <brunosilva33357@gmail.com>
2025-05-26 22:17:30 -0300

    Aprendemos mais sobre React

---
+++ ola/src/components/Body.jsx
@@ -0,0 +1,11 @@
+import './Body.css'
+function Body() {
+  return (
+    <div className='Body'>
+        <h2 className='titulo2'>Dino são legais</h2>
+            <p>Um dado curioso sobre dinossauros: a espécie Yutyrannus huali, um primo do T. Rex, era um dinossauro gigante (cerca de 9 metros de comprimento) e completamente coberto de penas, provavelmente para se manter aquecido.</p>
+        </div>
+  )
+}
+
+export default Body
